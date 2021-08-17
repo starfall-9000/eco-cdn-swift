d1fe55646d3dceee0f26ee4409aeed9eac7b0b52
starfall_9000 <starfall.9000.21@gmail.com>
2021-08-17 17:42:25 +0700

    integrate swarm cloud sdk

--- EcoCDN/ViewController.swift
+++ EcoCDN/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import AVKit
+import SwarmCloudSDK
 
 class ViewController: UIViewController {
+    @IBOutlet weak var container: UIView!
+    var player: AVPlayer? = nil
+    var layer: AVPlayerLayer? = nil
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        layer?.frame = CGRect(origin: .zero, size: container.frame.size)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupPlayer()
     }
-
-
+    
+    private func setupPlayer() {
+        guard let orginalUrl = URL(string: "https://content.jwplatform.com/manifests/vM7nH0Kl.m3u8")
+        else { return }
+        let parsedUrl = SWCP2pEngine.sharedInstance().parse(streamURL: orginalUrl)
+        player = AVPlayer.init(url: parsedUrl)
+        layer = AVPlayerLayer(player: player)
+        layer?.videoGravity = .resizeAspect
+        if let layer = layer {
+            container.layer.addSublayer(layer)
+        }
+        player?.play()
+    }
 }
 
