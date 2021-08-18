25866db3c3553f28cb5f9062cfc307b0bce72dc4
starfall_9000 <starfall.9000.21@gmail.com>
2021-08-18 12:03:16 +0700

    support iOS 10++

--- EcoCDN/AppDelegate.swift
+++ EcoCDN/AppDelegate.swift
@@ -10,9 +10,8 @@ import SwarmCloudSDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let config = SWCP2pConfig.defaultConfiguration()
@@ -20,21 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         SWCP2pEngine.sharedInstance().start(token: "luK3-_tMR", p2pConfig: config)
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 
 }
 
